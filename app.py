dfae961af8c097f356d9faddffd7fe224744d709
Brandon Benton <brandon.benton.5255@gmail.com>
2025-05-09 09:04:09 -0500

    post view

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import sqlite3
 from flask import Flask, render_template
 
+app = Flask(__name__)
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-app = Flask(__name__)
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
 
 @app.route('/')
 def index():
